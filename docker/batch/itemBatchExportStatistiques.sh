24f36c2eb6ca8d78f7b38df423eb01ffa3868d3f
pierre-maraval <pierre.maraval@gmail.com>
2023-01-24 10:53:08 +0100

    ITEM-17 : Fix : correction script shell batch

--- docker/batch/itemBatchExportStatistiques.sh
+++ docker/batch/itemBatchExportStatistiques.sh
@@ -1,12 +1,8 @@
-#!/usr/bin/env bash
-
 # dans la cron tab :
 # 30 1 1 * * /home/batch/Kopya/current/bin/itemBatchExportStatistiques.sh > /dev/null 2>&1
 
-cd /home/batch/item/current
 LANG=fr_FR.UTF-8
 if [[ $(pgrep -cf "itemBatch.jar --spring.batch.job.names=exportStatistiques") = 0 ]];
 then
-    echo "launch batch" > /home/batch/item/logs/item_batch_export_statistiques_last_launch.log
-    /usr/java/jdk11/bin/java -Djava.security.egd=file:///dev/urandom -jar itemBatch.jar --spring.batch.job.names=exportStatistiques --server.port=8081 >/dev/null 2>/home/batch/item/logs/item_batch_export_statistiques_error.log
+    java -jar itemBatch.jar --spring.batch.job.names=exportStatistiques
 fi
--- docker/batch/itemBatchStatutSupprimeDemandesPlusDeTroisMois.sh
+++ docker/batch/itemBatchStatutSupprimeDemandesPlusDeTroisMois.sh
@@ -1,12 +1,8 @@
-#!/usr/bin/env bash
-
 # dans la cron tab :
 # * * * * * /home/batch/item/current/bin/itemBatchStatutSupprimeDemandesPlusDeTroisMois.sh > /dev/null 2>&1
 
-cd /home/batch/item/current
 LANG=fr_FR.UTF-8
 if [[ $(pgrep -cf "itemBatch.jar --spring.batch.job.names=statutSupprimeDemandesPlusDeTroisMois") < 1 ]];
 then
-    echo "launch batch" > /home/batch/item/logs/item_batch_statut_supprime_demandes_last_launch.log
-    /usr/java/jdk11/bin/java -Djava.security.egd=file:///dev/urandom -jar itemBatch.jar --spring.batch.job.names=statutSupprimeDemandesPlusDeTroisMois >/dev/null 2>/home/batch/item/logs/item_batch_statut_supprime_demandes_error.log
+    java -jar itemBatch.jar --spring.batch.job.names=statutSupprimeDemandesPlusDeTroisMois
 fi
