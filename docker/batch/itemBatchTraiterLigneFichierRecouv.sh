30bc1eb6fa601dc5e9659519f4f24116adb01777
pierre-maraval <pierre.maraval@gmail.com>
2024-09-19 08:45:18 +0200

    FIX : test options JVM sur lancement des batch : ajout max ram utilisable

--- docker/batch/itemBatchTraiterLigneFichierRecouv.sh
+++ docker/batch/itemBatchTraiterLigneFichierRecouv.sh
@@ -4,5 +4,5 @@
 LANG=fr_FR.UTF-8
 if [[ $(pgrep -cf "item-batch.jar --spring.batch.job.name=traiterLigneFichierRecouv") < 1 ]];
 then
-    java -jar -XX:MaxRAMPercentage=80 -XX:+UseG1GC -Xshare:on -XX:+UseCompressedOops /scripts/item-batch.jar --spring.batch.job.name=traiterLigneFichierRecouv --server.port=8084
+    java -jar -XX:MaxRAMPercentage=80 -XX:+UseG1GC -Xshare:on -XX:+UseCompressedOops -Xmx1024m /scripts/item-batch.jar --spring.batch.job.name=traiterLigneFichierRecouv --server.port=8084
 fi
--- docker/batch/itemBatchTraiterLigneFichierSupp.sh
+++ docker/batch/itemBatchTraiterLigneFichierSupp.sh
@@ -4,5 +4,5 @@
 LANG=fr_FR.UTF-8
 if [[ $(pgrep -cf "item-batch.jar --spring.batch.job.name=traiterLigneFichierSupp") < 1 ]];
 then
-    java -jar -XX:MaxRAMPercentage=80 -XX:+UseG1GC -Xshare:on -XX:+UseCompressedOops /scripts/item-batch.jar --spring.batch.job.name=traiterLigneFichierSupp --server.port=8085
+    java -jar -XX:MaxRAMPercentage=80 -XX:+UseG1GC -Xshare:on -XX:+UseCompressedOops -Xmx1024m /scripts/item-batch.jar --spring.batch.job.name=traiterLigneFichierSupp --server.port=8085
 fi
