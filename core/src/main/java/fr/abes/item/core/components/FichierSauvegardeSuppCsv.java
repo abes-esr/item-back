03b9e3716ad9e8e244d51015877cd64d523841f6
EryneKL <97091460+EryneKL@users.noreply.github.com>
2024-10-01 14:12:26 +0200

    FEAT ITEM-235-batch-alimenter-le-fichier-de-sauvegarde-csv      - ajout d'un TU pour la méthode ConstructHeaderCsv      - ajout du début de l'algorithme d'alimentation du fichier csv

--- core/src/main/java/fr/abes/item/core/components/FichierSauvegardeSuppCsv.java
+++ core/src/main/java/fr/abes/item/core/components/FichierSauvegardeSuppCsv.java
@@ -1,6 +1,7 @@
 package fr.abes.item.core.components;
 
 import fr.abes.cbs.notices.Exemplaire;
+import fr.abes.cbs.notices.Zone;
 import fr.abes.item.core.constant.Constant;
 import fr.abes.item.core.constant.TYPE_DEMANDE;
 import fr.abes.item.core.entities.item.Demande;
@@ -29,7 +30,20 @@ public class FichierSauvegardeSuppCsv extends AbstractFichier implements Fichier
     }
 
     public void writePpnInFile(String ppn, List<Exemplaire> exemplaires) throws StorageException {
-        // TODO coder l'algo de remplissage du fichier csv (hors en-tête)
+        // TODO boucler sur chaque exemplaire et rechercher une correspondance entre une zone de l'exemplaire et la première zone du header, si correspondance alors mettre la zone exemplaire au bon index dans le csvContent
+        for (Exemplaire exemplaire : exemplaires) {
+            this.csvContent.append(ppn).append(";"); // ajout du ppn
+            for (String zoneAChercher : this.csvContent.toString().split(";")) {
+                List<Zone> listZones = exemplaire.getListeZones().values().stream().filter(exemp -> exemp.getLabel().equals(zoneAChercher)).toList(); // getLabel renvoi le numéro d'exemplaire (exemple e01)
+                for (Zone zone : listZones) {
+                    // TODO la zone et l'inclure dans le csvContent à la bonne place
+
+                }
+            }
+
+
+            this.csvContent.append("\n"); // retour à la ligne
+        }
     }
 
     @Override
