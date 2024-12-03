dc05daa3c46f8cbb9d736dce8d2561eae9af98bb
pierre-maraval <pierre.maraval@gmail.com>
2024-12-03 11:19:19 +0100

    Refactor : port java pour batch + nb de batch max

--- docker/batch/itemBatchTraiterLigneFichierModif.sh
+++ docker/batch/itemBatchTraiterLigneFichierModif.sh
@@ -2,7 +2,7 @@
 # * * * * * /home/batch/item/current/bin/itemBatchTraiterLigneFichierModif.sh > /dev/null 2>&1
 
 LANG=fr_FR.UTF-8
-if [[ $(pgrep -cf "traiterLigneFichierModif") < 1 ]];
+if [[ $(pgrep -cf "traiterLigneFichierModif") < 2 ]];
 then
-    java -XX:MaxRAMPercentage=80 org.springframework.boot.loader.JarLauncher --spring.batch.job.name=traiterLigneFichierModif --server.port=8083
+    java -XX:MaxRAMPercentage=80 org.springframework.boot.loader.JarLauncher --spring.batch.job.name=traiterLigneFichierModif --server.port=0
 fi
