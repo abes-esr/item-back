1ef75e28e3697dcdf3e0b915d75f4a5cd10df227
SamuelQuetin <samquetinuel@gmail.com>
2025-01-06 11:44:18 +0100

    ITEM-442 Ajout appel au back pour verifier s'il y a des demandes à traiter

--- docker/batch/itemBatchTraiterLigneFichierExemp.sh
+++ docker/batch/itemBatchTraiterLigneFichierExemp.sh
@@ -1,8 +1,11 @@
 # dans la cron tab :
 # * * * * * /home/batch/item/current/bin/itemBatchTraiterLigneFichierExemp.sh > /dev/null 2>&1
+url="${URL_BACK}/demandes/en-attente/EXEMP"
+
+isEnAttente=$(curl -s $url)
 
 LANG=fr_FR.UTF-8
-if [[ $(pgrep -cf "traiterLigneFichierExemp") < 2 ]];
+if [[ $isEnAttente == 'true' &&  $(pgrep -cf "traiterLigneFichierExemp") < 2 ]];
 then
    java -XX:MaxRAMPercentage=80 org.springframework.boot.loader.JarLauncher --spring.batch.job.name=traiterLigneFichierExemp --server.port=0
 fi
--- docker/batch/itemBatchTraiterLigneFichierRecouv.sh
+++ docker/batch/itemBatchTraiterLigneFichierRecouv.sh
@@ -1,8 +1,11 @@
 # dans la cron tab :
 # * * * * * /home/batch/item/current/bin/itemBatchTraiterLigneFichierRecouv.sh > /dev/null 2>&1
+url="${URL_BACK}/demandes/en-attente/RECOUV"
+
+isEnAttente=$(curl -s $url)
 
 LANG=fr_FR.UTF-8
-if [[ $(pgrep -cf "traiterLigneFichierRecouv") < 2 ]];
+if [[ $isEnAttente == 'true' && $(pgrep -cf "traiterLigneFichierRecouv") < 2 ]];
 then
     java -XX:MaxRAMPercentage=80 org.springframework.boot.loader.JarLauncher --spring.batch.job.name=traiterLigneFichierRecouv --server.port=0
 fi
