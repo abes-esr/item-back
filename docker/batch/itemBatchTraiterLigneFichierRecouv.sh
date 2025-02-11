2e9c86230b746898b43cdebe56817e8a7c85fc8f
SamuelQuetin <quetin@abes.fr>
2025-02-11 15:26:09 +0100

    ITEM-496 remettre 5000 et remettre deux canaux en parallele /!\ pour pouvoir garder la features j'ai mis en commentaire les deux canaux big et small en commentaire et j'ai augmenté drastiquement la limite de bigVolume

--- docker/batch/itemBatchTraiterLigneFichierRecouv.sh
+++ docker/batch/itemBatchTraiterLigneFichierRecouv.sh
@@ -9,14 +9,25 @@ isEnAttente=$(curl -s $urlIsEnAttente)
 LANG=fr_FR.UTF-8
 if [[ $isEnAttente == 'true' ]];
 then
-  isEnAttenteBigVolume=$(curl -s $urlIsEnAttenteBigVolume)
-  isEnAttenteSmallVolume=$(curl -s $urlIsEnAttenteSmallVolume)
-  if [[ $isEnAttenteBigVolume == 'true' && $(pgrep -cf "traiterLigneFichierRecouv --bigVolume=true") < 1 ]];
+  #########################################################
+  # Cas avec Deux canaux distinct [Big] & [Small]
+  #########################################################
+#  isEnAttenteBigVolume=$(curl -s $urlIsEnAttenteBigVolume)
+#  isEnAttenteSmallVolume=$(curl -s $urlIsEnAttenteSmallVolume)
+#  if [[ $isEnAttenteBigVolume == 'true' && $(pgrep -cf "traiterLigneFichierRecouv --bigVolume=true") < 1 ]];
+#  then
+#   java -XX:MaxRAMPercentage=80 org.springframework.boot.loader.JarLauncher --spring.batch.job.name=traiterLigneFichierRecouv --bigVolume=true --server.port=0 &
+#  fi
+#  if [[ $isEnAttenteSmallVolume == 'true' && $(pgrep -cf "traiterLigneFichierRecouv --bigVolume=false") < 1 ]];
+#  then
+#   java -XX:MaxRAMPercentage=80 org.springframework.boot.loader.JarLauncher --spring.batch.job.name=traiterLigneFichierRecouv --bigVolume=false --server.port=0 &
+#  fi
+
+  #########################################################
+  # Cas avec Deux canaux identique
+  #########################################################
+  if [[ $(pgrep -cf "traiterLigneFichierRecouv") -lt 2 ]];
   then
-   java -XX:MaxRAMPercentage=80 org.springframework.boot.loader.JarLauncher --spring.batch.job.name=traiterLigneFichierRecouv --bigVolume=true --server.port=0 &
-  fi
-  if [[ $isEnAttenteSmallVolume == 'true' && $(pgrep -cf "traiterLigneFichierRecouv --bigVolume=false") < 1 ]];
-  then
-   java -XX:MaxRAMPercentage=80 org.springframework.boot.loader.JarLauncher --spring.batch.job.name=traiterLigneFichierRecouv --bigVolume=false --server.port=0 &
+    java -XX:MaxRAMPercentage=80 org.springframework.boot.loader.JarLauncher --spring.batch.job.name=traiterLigneFichierRecouv --server.port=0 &
   fi
 fi
